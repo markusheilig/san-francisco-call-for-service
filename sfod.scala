1a91c5c1e017fbce06e5a6501b94942c26999925
Markus Heilig <markusheilig@gmx.net>
2018-04-13 13:17:38 +0200

    finish lr model
    + accuracy: 0.7663245514789074
    
    + confusionMatrix:
    156049.0  123422.0
    3804.0      261181.0

--- sfod.scala
+++ sfod.scala
@@ -10,6 +10,29 @@ val spark = SparkSession.builder().getOrCreate()
 val csvFile = "/Volumes/TranscendJetdriveLite330/downloads/Fire_Department_Calls_for_Service.csv"
 var df = spark.read.option("header", "true").option("inferSchema", "true").csv(csvFile)
 
+// data processing - rename columns (remove spaces and dashes)
+val newColumnNames = df.columns.map(columnName => columnName.replace(" ", "").replace("-", ""))
+df = df.toDF(newColumnNames: _*)
+
+// data processing - remove all entries where label or feature columns are null
+df = df.na.drop(Array("CallTypeGroup", "ZipcodeofIncident", "CallType", "UnitType", "NeighborhooodsAnalysisBoundaries", "NumberofAlarms", "EntryDtTm"))
+
+// data processing - date schema is not detected automatically -> convert string to datetime
+import org.apache.spark.sql.functions.to_timestamp
+df = df.withColumn("EntryDtTm", to_timestamp($"EntryDtTm", "MM/dd/yyyy hh:mm:ss a"))
+
+// data processing - get hour of incoming call
+df = df.withColumn("EntryHour", hour($"EntryDtTm"))
+
+// data processing - get quarter of incoming call
+df = df.withColumn("EntryQuarter", quarter($"EntryDtTm"))
+
+// data processing - drop columns where there is no time specification
+df = df.na.drop(Array("EntryHour", "EntryQuarter"))
+
+// data processing - add the label column (label = 1 <==> CallTypeGroup = "Potentially Life-Threatening" otherwise label = 0)
+df = df.withColumn("label", when($"CallTypeGroup" === "Potentially Life-Threatening", 1).otherwise(0))
+
 // data exploration - print schema
 df.printSchema()
 
@@ -23,29 +46,55 @@ df.printSchema()
   }
 }
 
-// data processing - rename columns (remove spaces)
-val columnNamesWithoutSpaces = df.columns.map(columnName => columnName.replace(" ", ""))
-df = df.toDF(columnNamesWithoutSpaces: _*)
-
-// data processing - remove all entries where 'CallTypeGroup' is null
-df = df.na.drop(Array("CallTypeGroup"))
-
-// data processing - date schema is not detected automatically
-import org.apache.spark.sql.functions.to_timestamp
-df = df.withColumn("EntryDtTm", to_timestamp($"EntryDtTm", "MM/dd/yyyy hh:mm:ss a"))
-
-// todo:
-// EntryDtTm enthält komplettes datum, evtl. ist nur die aktuelle stunde wichtig (hh)
-// zudem spielt evtl. der wochentag noch eine rolle? -> neue spalte erzeugen?
-
-// data processing - add feature label (Feature = 1 <==> CallTypeGroup = "Potentially Life-Threatening" otherwise Feature = 0)
-df = df.withColumn("Feature", when($"CallTypeGroup" === "Potentially Life-Threatening", 1).otherwise(0))
+// data exploration - how many incidents are "Potentially Life-Threatening" (in percent)
+{  
+  val potentiallyLifeThreatening = df.filter($"label" === 1).count()  
+  val percentage = potentiallyLifeThreatening.toDouble / df.count()
+  print("%.2f%% of all incdidents are potentially life-threatening".format(percentage * 100))
+}
 
 // data processing - create String Indexer for categorical values
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
-df = new StringIndexer().setInputCol("CallTypeGroup").setOutputCol("CallTypeGroupIndex").fit(df).transform(df)
-df = new StringIndexer().setInputCol("CallType").setOutputCol("CallTypeIndex").fit(df).transform(df)
+val callTypeGroupIndexer = new StringIndexer().setInputCol("CallTypeGroup").setOutputCol("CallTypeGroupIndex").setHandleInvalid("keep")
+val callTypeIndexer = new StringIndexer().setInputCol("CallType").setOutputCol("CallTypeIndex").setHandleInvalid("keep")
+val unitTypeIndexer = new StringIndexer().setInputCol("UnitType").setOutputCol("UnitTypeIndex").setHandleInvalid("keep")
+val neighborhooodsAnalysisBoundariesIndexer = new StringIndexer().setInputCol("NeighborhooodsAnalysisBoundaries").setOutputCol("NeighborhooodsAnalysisBoundariesIndex").setHandleInvalid("keep")
+val indexers = Array(callTypeGroupIndexer, callTypeIndexer, unitTypeIndexer, neighborhooodsAnalysisBoundariesIndexer)
 
 // data processing - create One Hot Encoder for categorical values
-df = new OneHotEncoder().setInputCol("CallTypeGroupIndex").setOutputCol("CallTypeGroupVec").transform(df)
-df = new OneHotEncoder().setInputCol("CallTypeIndex").setOutputCol("CallTypeVec").transform(df)
+val callTypeGroupEncoder = new OneHotEncoder().setInputCol("CallTypeGroupIndex").setOutputCol("CallTypeGroupVec")
+val callTypeEncoder = new OneHotEncoder().setInputCol("CallTypeIndex").setOutputCol("CallTypeVec")
+val unitTypeEncoder = new OneHotEncoder().setInputCol("UnitTypeIndex").setOutputCol("UnitTypeVec")
+val neighborhooodsAnalysisBoundariesEncoder = new OneHotEncoder().setInputCol("NeighborhooodsAnalysisBoundariesIndex").setOutputCol("NeighborhooodsAnalysisBoundariesVec")
+val encoders = Array(callTypeGroupEncoder, callTypeEncoder, unitTypeEncoder, neighborhooodsAnalysisBoundariesEncoder)
+
+// (label, features)
+import org.apache.spark.ml.feature.VectorAssembler
+val features = Array("ZipcodeofIncident", "CallTypeVec", "UnitTypeVec", "NeighborhooodsAnalysisBoundariesVec", "EntryHour", "EntryQuarter", "NumberofAlarms")
+val assembler = new VectorAssembler().setInputCols(features).setOutputCol("features")
+val Array(training, test) = df.randomSplit(Array(0.7, 0.3), seed=12345)
+
+// setup pipeline
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
+
+val lr = new LogisticRegression()
+val stages = indexers ++ encoders :+ assembler :+ lr
+val pipeline = new Pipeline().setStages(stages)
+
+// train
+val model = pipeline.fit(training)
+
+// test
+val results = model.transform(test)
+
+// model evaluation
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+
+val predictionAndLabels = results.select($"prediction", $"label").as[(Double, Double)].rdd
+
+val metrics = new MulticlassMetrics(predictionAndLabels)
+println("Accuracy: ")
+println(metrics.accuracy)
+println("Confusion matrix: ")
+println(metrics.confusionMatrix)
