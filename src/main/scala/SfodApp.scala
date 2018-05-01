862c7cc30d474fcda2e1bed94e2dbfac34d1e845
Philip Schaefer <00philip00@gmail.com>
2018-05-01 13:35:46 +0200

    MLP 52%, RandomForest 90%, LogisticRegression 52%

--- src/main/scala/SfodApp.scala
+++ src/main/scala/SfodApp.scala
@@ -1,10 +1,12 @@
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.classification.{LogisticRegression, MultilayerPerceptronClassifier, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.IntegerType
 
 // dataset can be found at https://data.sfgov.org/Public-Safety/Fire-Department-Calls-for-Service/nuek-vuh3
 object SfodApp {
@@ -28,14 +30,13 @@ object SfodApp {
     df = df.toDF(newColumnNames: _*)
 
     // data processing - remove all entries where label or feature columns are null
-    df = df.na.drop(Array("CallType","Priority", "ZipcodeofIncident"))
-    
+    df = df.na.drop(Array("CallType", "Priority", "ZipcodeofIncident"))
+
     // data processing - date schema is not detected automatically -> convert string to datetime
     df = df.withColumn("EntryDtTm", to_timestamp($"EntryDtTm", "MM/dd/yyyy hh:mm:ss a"))
-
     df = df.withColumn("HourOfDay", hour($"EntryDtTm"))
     df = df.withColumn("DayOfWeek", dayofweek($"EntryDtTm"))
-    df = df.withColumn("isWeekend", dayofweek($"EntryDtTm")>=5)
+    df = df.withColumn("isWeekend", dayofweek($"EntryDtTm") >= 5)
     df = df.withColumn("MonthOfYear", month($"EntryDtTm"))
     df = df.withColumn("WeekOfYear", weekofyear($"EntryDtTm"))
 
@@ -44,6 +45,9 @@ object SfodApp {
 
     // data processing - add the label column (label = 1 <==> CallTypeGroup = "Potentially Life-Threatening" otherwise label = 0)
     df = df.withColumn("label", when($"CallTypeGroup" === "Potentially Life-Threatening", 1).otherwise(0))
+
+    //There are no same incidentNumbers where CallTypeGroup or Priority is different (BUT slower and worse)
+    //df = df.dropDuplicates("IncidentNumber")
 
     val Array(train, test) = df.randomSplit(Array(0.7, 0.3), seed = 12345)
 
@@ -68,58 +72,64 @@ object SfodApp {
     }
 
     // data processing - create String Indexer for categorical values
-    val (indexers, encoders) = Helper.indexAndEncode("CallType", "Box", "Priority", "ZipcodeofIncident")
-    val featureNames = Array("DayOfWeek","MonthOfYear","isWeekend", "HourOfDay", "WeekOfYear") ++ indexers.map(_.getOutputCol) //++ encoders.flatMap(_.getOutputCols)
+    val (indexers, encoders) = Helper.indexAndEncode("CallType", "Priority", "NeighborhooodsAnalysisBoundaries")
+    val featureNames = Array("HourOfDay", "DayOfWeek", "isWeekend", "WeekOfYear", "MonthOfYear", "ZipcodeofIncident") ++ indexers.map(_.getOutputCol) //++ encoders.flatMap(_.getOutputCols)
     val assembler = new VectorAssembler().setInputCols(featureNames).setOutputCol("features")
 
-    // setup pipeline
+    //Prepare Stages
+    val stages = indexers ++ encoders :+ assembler
 
-    /*
-    val lr = new LogisticRegression()
-    val stages = indexers ++ encoders :+ assembler :+ lr
-    val pipeline = new Pipeline().setStages(stages)
-
-    // train
-    val model = pipeline.fit(train)
-
-    // test
-    val results = model.transform(test)
-
-    // model evaluation
-    val predictionAndLabels = results.select($"prediction", $"label").as[(Double, Double)].rdd
-
-    val metrics = new MulticlassMetrics(predictionAndLabels)
-    println("Accuracy: ")
-    println(metrics.accuracy)
-    println("Confusion matrix: ")
-    println(metrics.confusionMatrix)
-    */
-
-
-    //val features = assembler.transform(df)
-    val xxx = indexers ++ encoders :+ assembler
-    val p = new Pipeline().setStages(xxx)
-    val f = p.fit(df)
-    f.transform(df).show(20, false)
-
-    //val newTrainingData = train.select($"label", $"features")
-    //val newTestData = test.select($"label", $"features")
+    //Show pre statistics
+    val testPipe = new Pipeline().setStages(stages)
+    val testModel = testPipe.fit(df)
+    val testFrame = testModel.transform(df)
+    testFrame.show(20, false)
+    //testFrame.select($"Box").distinct().show(3000, false)
 
     val numFeatures = featureNames.length
     println("numFeatures --> " + numFeatures)
     println("trainCount --> " + train.count())
     println("testCount --> " + test.count())
-    val layers = Array[Int](numFeatures, 7, 7 , 2)
-    val trainer = new MultilayerPerceptronClassifier()
-      .setLayers(layers)
-      .setBlockSize(128)
-      .setSeed(1234L)
-      .setMaxIter(100)
 
-    val stages = indexers :+ assembler :+ trainer
-    val pipeline = new Pipeline().setStages(stages)
+    // Classifiers
+    def randomForest(): PipelineStage ={
 
-    // train the model
+      //Find feature with max count for maxBins
+      var max:Long = 0
+      var count:Long = 0
+      featureNames.foreach(feature => {
+        count = testFrame.agg(countDistinct(feature).as("count")).collectAsList().get(0).getAs[Long](0)
+        if (max < count) {
+          max = count;
+        }
+      })
+
+      return new RandomForestClassifier()
+        .setImpurity("gini")
+        .setMaxDepth(3)
+        .setNumTrees(20)
+        .setFeatureSubsetStrategy("auto")
+        .setSeed(5043)
+        .setMaxBins(max.toInt)
+    }
+
+    def MLP(): PipelineStage ={
+      val layers = Array[Int](numFeatures, 7, 7 , 2)
+      return new MultilayerPerceptronClassifier()
+        .setLayers(layers)
+        .setBlockSize(128)
+        .setSeed(1234L)
+        .setMaxIter(100)
+    }
+
+    def LogisticRegression(): PipelineStage ={
+      return new LogisticRegression()
+    }
+
+
+    val classifier = randomForest()
+    //Setup pipeline
+    val pipeline = new Pipeline().setStages(stages :+ classifier)
     val model = pipeline.fit(train)
 
     def printStatistics(what: String, result: DataFrame): Unit = {
@@ -135,7 +145,5 @@ object SfodApp {
 
     val testResult = model.transform(test)
     printStatistics("Test", testResult)
-
   }
-
 }
