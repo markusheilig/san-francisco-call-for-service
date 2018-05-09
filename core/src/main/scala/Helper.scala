677a251605255bbcb201a4b9097a7fe8f69ab946
Markus Heilig <markusheilig@gmx.net>
2018-05-09 12:00:48 +0200

    clean up

--- core/src/main/scala/Helper.scala
+++ core/src/main/scala/Helper.scala
@@ -1,24 +1,12 @@
 import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.OneHotEncoderEstimator
 
 object Helper {
 
-  /**
-   * Create StringIndexer and OneHotEncoderEstimators for the given columns
-   * @param columns Category columns for which indexers and estimators will be created.
-   * @return Tuple of indexers and estimators.
-   */
-  def indexAndEncode(columns: String*): (Array[StringIndexer], Array[OneHotEncoderEstimator]) = {
-    val indexer = columns.map(createIndexer).toArray
-    val encoders = columns.map(createEncoder).toArray
-    (indexer, encoders)
+  def index(columns: String*): Array[StringIndexer] = {
+    columns.map(createIndexer).toArray
   }
 
   private def createIndexer(column: String): StringIndexer = {
     new StringIndexer().setInputCol(column).setOutputCol(s"${column}Index").setHandleInvalid("keep")
-  }
-
-  private def createEncoder(column: String): OneHotEncoderEstimator = {
-    new OneHotEncoderEstimator().setInputCols(Array(s"${column}Index")).setOutputCols(Array(s"${column}Vec"))
   }
 }
--- core/src/main/scala/SfodApp.scala
+++ core/src/main/scala/SfodApp.scala
@@ -20,32 +20,20 @@ object SfodApp {
     // read dataset
     // dataset can be found at https://data.sfgov.org/Public-Safety/Fire-Department-Calls-for-Service/nuek-vuh3
     val csvFile = "./../datasets/100k.csv"
-    // dataset can be found at https://data.sfgov.org/Public-Safety/Fire-Incidents/wr8u-xric
-    val fireCsv = "./../datasets/Fire_Incidents_100k.csv"
-
     var df = spark.read.option("header", "true").option("inferSchema", "true").csv(csvFile)
-    var fire_incidents = spark.read.option("header", "true").option("inferSchema", "true").csv(fireCsv)
-
-    //df = df.join(fire_incidents, df("Incident Number") === fire_incidents("Incident Number"))
 
     // data processing - rename columns (remove spaces and dashes)
     val newColumnNames = df.columns.map(_.replace(" ", "").replace("-", ""))
     df = df.toDF(newColumnNames: _*)
 
     // data processing - remove all entries where label or feature columns are null
-    df = df.na.drop(Array("CallType", "Priority", "CallTypeGroup"))
+    df = df.na.drop(Array("CallType", "Priority", "CallTypeGroup", "EntryDtTm"))
 
     // data processing - date schema is not detected automatically -> convert string to datetime
     df = df.withColumn("EntryDtTm", to_timestamp($"EntryDtTm", "MM/dd/yyyy hh:mm:ss a"))
     df = df.withColumn("HourOfDay", hour($"EntryDtTm"))
     df = df.withColumn("DayOfYear", dayofyear($"EntryDtTm"))
-    df = df.withColumn("isWeekend", dayofweek($"EntryDtTm") >= 5)
-    df = df.withColumn("MonthOfYear", month($"EntryDtTm"))
-    df = df.withColumn("WeekOfYear", weekofyear($"EntryDtTm"))
-
-    // data processing - drop columns where there is no time specification
-    df = df.na.drop(Array("HourOfDay", "isWeekend", "MonthOfYear", "WeekOfYear"))
-
+    df = df.na.drop(Array("HourOfDay", "DayOfYear"))
 
     // Introduce geohash
     val pattern = "-?\\d+\\.{1}\\d+".r
@@ -55,22 +43,19 @@ object SfodApp {
       GeoHash.geoHashStringWithCharacterPrecision(coords.apply(0).toDouble, coords.apply(1).toDouble, 6)
     })
     df = df.withColumn("geohash", callUDF("geohash", $"Location"))
-    df.show(20, false)
-
-    /*{
-      df.createOrReplaceTempView("incidents")
-      println("Different isCriticalDispo : " + spark.sql("SELECT i1x.IncidentNumber, i2x.IncidentNumber FROM incidents as i1x INNER JOIN incidents as i2x ON i1x.IncidentNumber = i2x.IncidentNumber AND i1x.isCriticalDisposition != i2x.isCriticalDisposition ").count())
-    }*/
 
     {
+      // define our label
+
       def cdfUDF = udf((s:String) => {
         val criticalDispositions = Set("Code 2 Transport", "Fire", "Patient Declined Transport", "Against Medical Advice", "Medical Examiner")
-        if (criticalDispositions.contains(s)) true else false
+        criticalDispositions.contains(s)
       } )
 
       def hospitalUDF = udf((hospital: String) => {
         if (hospital == null || hospital.isEmpty) false else true
       } )
+
       df = df.withColumn("isCriticalDispositionT", cdfUDF(df("CallFinalDisposition")))
         .withColumn("isHospitalTransportT", hospitalUDF(df("HospitalDtTm")))
 
@@ -81,44 +66,21 @@ object SfodApp {
         .withColumn("isHospitalTransport", array_contains('tmpH, true))
         .drop("tmpCD").drop("tmpH")
       df = df.join(dfTemp, Seq("IncidentNumber"))
+
+      // data processing - add the label column (label = 1 <==> CallTypeGroup = "Potentially Life-Threatening" otherwise label = 0)
+      df = df.withColumn("label", when($"CallTypeGroup" === "Potentially Life-Threatening" && ($"isCriticalDisposition" === true || $"isHospitalTransport" === true), 1).otherwise(0))
     }
 
-    /*{
-      df.createOrReplaceTempView("incidents")
-      println("Different isCriticalDispo : " + spark.sql("SELECT i1.IncidentNumber, i2.IncidentNumber FROM incidents as i1 INNER JOIN incidents as i2 ON i1.IncidentNumber = i2.IncidentNumber AND i1.isCriticalDisposition != i2.isCriticalDisposition ").count())
-    }*/
-
-
-    //df.show(20, false)
-
-    // data processing - add the label column (label = 1 <==> CallTypeGroup = "Potentially Life-Threatening" otherwise label = 0)
-    df = df.withColumn("label", when($"CallTypeGroup" === "Potentially Life-Threatening" && ($"isCriticalDisposition" === true || $"isHospitalTransport" === true), 1).otherwise(0))
-
-    //There are no same incidentNumbers where CallTypeGroup or Priority is different (BUT slower and worse)
-    //df = df.dropDuplicates("IncidentNumber")
 
     val Array(train, test) = df.randomSplit(Array(0.9, 0.1), seed = 12345)
 
-    // data exploration - print schema
-    df.printSchema()
-
-    // data exploration - pretty print first row
-    df.show(1, false)
-
-    // data exploration - how many incidents are "Potentially Life-Threatening" (in percent)
-    {
-      val potentiallyLifeThreatening = df.filter($"label" === 1).count()
-      val percentage = potentiallyLifeThreatening.toDouble / df.count()
-      println("%.2f%% of all incidents are potentially life-threatening".format(percentage * 100))
-    }
-
     // data processing - create String Indexer for categorical values
-    val (indexers, encoders) = Helper.indexAndEncode("CallType", "Priority", "NeighborhooodsAnalysisBoundaries")
-    val featureNames = Array("HourOfDay", "DayOfYear" /*,"isWeekend" "WeekOfYear", "MonthOfYear"*/) ++ indexers.map(_.getOutputCol) //++ encoders.flatMap(_.getOutputCols)
+    val indexers = Helper.index("CallType", "Priority", "NeighborhooodsAnalysisBoundaries")
+    val featureNames = Array("HourOfDay", "DayOfYear") ++ indexers.map(_.getOutputCol)
     val assembler = new VectorAssembler().setInputCols(featureNames).setOutputCol("features")
 
     //Prepare Stages
-    val stages = indexers ++ encoders :+ assembler
+    val stages = indexers :+ assembler
 
     //Print Features + Lable table
     val testPipe = new Pipeline().setStages(stages)
@@ -127,6 +89,13 @@ object SfodApp {
     var columns: Array[Column] = featureNames.map(testFrame(_))
     columns = columns :+ testFrame("label")
     testFrame.select(columns: _*).show(20, false)
+
+    // data exploration - how many incidents are "Potentially Life-Threatening" (in percent)
+    {
+      val potentiallyLifeThreatening = df.filter($"label" === 1).count()
+      val percentage = potentiallyLifeThreatening.toDouble / df.count()
+      println("%.2f%% of all incidents are potentially life-threatening".format(percentage * 100))
+    }
 
     //Show pre statistics
     val numFeatures = featureNames.length
@@ -139,12 +108,9 @@ object SfodApp {
 
       //Find feature with max count for maxBins
       var max: Long = 0
-      var count: Long = 0
       featureNames.foreach(feature => {
-        count = testFrame.agg(countDistinct(feature).as("count")).collectAsList().get(0).getAs[Long](0)
-        if (max < count) {
-          max = count
-        }
+        val count = testFrame.agg(countDistinct(feature).as("count")).collectAsList().get(0).getAs[Long](0)
+        max = Math.max(max, count)
       })
 
       new RandomForestClassifier()
@@ -153,39 +119,34 @@ object SfodApp {
         .setNumTrees(30)
         .setFeatureSubsetStrategy("auto")
         .setSeed(5043)
-        .setMaxBins(max.toInt /*+10*/)
+        .setMaxBins(max.toInt)
     }
 
-    def MLP(): PipelineStage = {
+    def multiLayerPerceptron(): PipelineStage = {
       val layers = Array[Int](numFeatures, 3, 2)
-      return new MultilayerPerceptronClassifier()
+      new MultilayerPerceptronClassifier()
         .setLayers(layers)
         .setBlockSize(128)
         .setSeed(1234L)
         .setMaxIter(100)
     }
 
-    def LogisticRegression(): PipelineStage = {
-      return new LogisticRegression()
-    }
-
-    def svm(): PipelineStage = {
-      new LinearSVC().setMaxIter(10).setRegParam(0.1)
+    def logisticRegression(): PipelineStage = {
+      new LogisticRegression()
     }
 
     val classifier = randomForest()
+
     //Setup pipeline
     val pipeline = new Pipeline().setStages(stages :+ classifier)
     val model = pipeline.fit(train)
 
-    {
-      // save model to file
-      //val timestamp = System.currentTimeMillis / 1000
-      //model.save(s"models/myclassifier-$timestamp")
-    }
+    // save model to file
+    //val timestamp = System.currentTimeMillis / 1000
+    //model.save(s"models/myclassifier-$timestamp")
 
-    def printStatistics(what: String, result: DataFrame): Unit = {
-      print(s"\n\n$what statistics:\n")
+    def printStatistics(resultType: String, result: DataFrame): Unit = {
+      print(s"\n\n$resultType statistics:\n")
       val predictionAndLabels = result.select("prediction", "label")
       val metrics = new MulticlassMetrics(predictionAndLabels.as[(Double, Double)].rdd)
       val FPR = metrics.falsePositiveRate(0)
